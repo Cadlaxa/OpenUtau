4752b2b40280c463053b5b5d1c02e5ea85974644
cadlaxa <92255161+Cadlaxa@users.noreply.github.com>
2026-09-10 09:48:56 +0800

    enable phonemeTokenization for en2ja

--- OpenUtau.Plugin.Builtin/ENtoJAPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/ENtoJAPhonemizer.cs
@@ -26,7 +26,7 @@ namespace OpenUtau.Plugin.Builtin {
         protected override string[] GetVowels() => vowels;
         protected override string[] GetConsonants() => consonants;
         protected override string GetDictionaryName() => "";
-        protected override bool EnablePhonemeTokenization => true;
+b        protected override bool EnablePhonemeTokenization => true;
 
         public Dictionary<string, List<string>> WanaKanaDictionary = new Dictionary<string, List<string>>();
 
