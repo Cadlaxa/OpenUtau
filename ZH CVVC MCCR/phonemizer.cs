0f0c081806c75ad64cb3c24ed5e84e4cfb1191c6
cadlaxa <cadag113003@gmail.com>
2026-09-22 16:57:26 +0800

    fix typo

--- ZH CVVC MCCR/phonemizer.cs
+++ ZH CVVC MCCR/phonemizer.cs
@@ -17,7 +17,7 @@ namespace OpenUtau.Plugin.Builtin {
     [Phonemizer("MCCR Mandarin Chinese Phonemizer", "ZH CVVC", "Cadlaxa", language: "ZH")]
     public class MandarinPhonemizer : SyllableBasedPhonemizer {
         protected override string YamlFileName => "zh-cvvc-mccr.yaml";
-        protected override string YamlVersion => "1.1.6";
+        protected override string YamlVersion => "1.1.7";
         protected override byte[] YamlTemplate => ZH_CVVC_MCCR.data.Resources.template;
         public MandarinPhonemizer() {
             this.vowels = Array.Empty<string>();
